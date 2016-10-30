ec166d1acd55679d659a77d39d55ed7a433d4fe4
Joe Cloud <im@joe.cloud>
2016-10-29 20:03:53 -0500

    file upload fixed

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 import os
-from flask import Flask, request, render_template, redirect, url_for
-from werkzeug.utils import secure_filename
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = set(['txt'])
+
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route("/")
-def _form():
-    return render_template("form.html")
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return  render_template("form.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=34448)
+    app.run(host='0.0.0.0', port=80, debug=False)
