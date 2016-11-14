733a0a7a1633f2dd388e24415a64247df06fd7b0
Joe Cloud <im@joe.cloud>
2016-11-14 12:48:06 -0600

    fixes

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def download():
 	return """<html> <body> <p> here is a nice string for ya </p </body> </html>"""
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=1255, debug=True)
