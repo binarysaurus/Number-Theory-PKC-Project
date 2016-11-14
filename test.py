aa7732a23d6b585ea46da73ffd716d9cf655627e
Joe Cloud <im@joe.cloud>
2016-11-14 13:23:26 -0600

    fixes

--- test.py
+++ test.py
@@ -31,40 +31,40 @@ def main():
  
     print(form.errors)
     if request.method == 'POST':
-    	if (request.form['str_encrypt'] != None):
- 	       str_encrypt=request.form['str_encrypt']
- 	       print(str_encrypt)
- 	
- 	       if form.validate():
- 	           # Save the comment here.
- 	           encrypted_data = encryptor(int(str_encrypt))
- 	           flash('Encrypted Text: '+str(encrypted_data[0]))
- 	           flash('Key:            '+str(encrypted_data[1]))
- 	       else:
- 	           flash('All the form fields are required. ')
-		else (request.form['str_decrypt'] != None):
- 	       str_decrypt=request.form['str_decrypt']
- 	       print(str_decrypt)
- 	
- 	       if form.validate():
- 	           # Save the comment here.
- 	           decrypted_data = decryptor(int(str_decrypt))
- 	           flash('Decrypted Text: '+str(decrypted_data))
- 	       else:
- 	           flash('All the form fields are required. ') 	    	
+        if (request.form['str_encrypt'] != None):
+            str_encrypt=request.form['str_encrypt']
+            print(str_encrypt)
+     
+            if form.validate():
+                # Save the comment here.
+                encrypted_data = encryptor(int(str_encrypt))
+                flash('Encrypted Text: '+str(encrypted_data[0]))
+                flash('Key:            '+str(encrypted_data[1]))
+            else:
+                flash('All the form fields are required. ')
+        else (request.form['str_decrypt'] != None):
+            str_decrypt=request.form['str_decrypt']
+            print(str_decrypt)
+     
+            if form.validate():
+                # Save the comment here.
+                decrypted_data = decryptor(int(str_decrypt))
+                flash('Decrypted Text: '+str(decrypted_data))
+            else:
+                flash('All the form fields are required. ')             
  
     return render_template('index.html', form=form)
 
 def is_prime(num):
-	if num > 1:
-		for i in range(2,num):
-			if (num % i) == 0:
-				return False;
-				break
-		else:
-			return True;
-	else:
-		return False;
+    if num > 1:
+        for i in range(2,num):
+            if (num % i) == 0:
+                return False;
+                break
+        else:
+            return True;
+    else:
+        return False;
 
 #Extended euclidean algorithm from:
 # https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm
@@ -78,56 +78,56 @@ def xgcd(b, n):
     return  b, x0, y0
 
 def encryptor(m):
-	p = 2**31-1
-	q = 2**61-1
-	n = p * q
-	phi = (p-1)*(q-1)
-	
-	e = randint(0,phi)
-	while gcd(e, phi) != 1:
-		e = randint(0,phi)
-	
-	bezout = xgcd(e, phi)
-	
-	d = bezout[1]%phi
-	
-	#words = 'sda'
-	#print("ORIGINAL STRING: ", words)
-	#m = toBinary(words)
-	#print("WORDS TO BINARY: ", m)	
-	#decword = toString(m)
-	#print("DECWORD  DECODE: ",decword)
-	#print("WORDS TO BINARY: ",m)
-	print("ORIGINAL BEFR: ", m)
-	#m = int(input("enter an int: "))
+    p = 2**31-1
+    q = 2**61-1
+    n = p * q
+    phi = (p-1)*(q-1)
+    
+    e = randint(0,phi)
+    while gcd(e, phi) != 1:
+        e = randint(0,phi)
+    
+    bezout = xgcd(e, phi)
+    
+    d = bezout[1]%phi
+    
+    #words = 'sda'
+    #print("ORIGINAL STRING: ", words)
+    #m = toBinary(words)
+    #print("WORDS TO BINARY: ", m)    
+    #decword = toString(m)
+    #print("DECWORD  DECODE: ",decword)
+    #print("WORDS TO BINARY: ",m)
+    print("ORIGINAL BEFR: ", m)
+    #m = int(input("enter an int: "))
 
-	c = pow(m,e,n)
-	print("CIPHERTEXT ENC : ", c)
-	return c, e
+    c = pow(m,e,n)
+    print("CIPHERTEXT ENC : ", c)
+    return c, e
 
 def decryptor(m, e):
-	p = 2**31-1
-	q = 2**61-1
-	n = p * q
-	phi = (p-1)*(q-1)
-	
-	bezout = xgcd(e, phi)
-	
-	d = bezout[1]%phi
-	
-	#words = 'sda'
-	#print("ORIGINAL STRING: ", words)
-	#m = toBinary(words)
-	#print("WORDS TO BINARY: ", m)	
-	#decword = toString(m)
-	#print("DECWORD  DECODE: ",decword)
-	#print("WORDS TO BINARY: ",m)
-	print("ORIGINAL BEFR: ", m)
-	#m = int(input("enter an int: "))
+    p = 2**31-1
+    q = 2**61-1
+    n = p * q
+    phi = (p-1)*(q-1)
+    
+    bezout = xgcd(e, phi)
+    
+    d = bezout[1]%phi
+    
+    #words = 'sda'
+    #print("ORIGINAL STRING: ", words)
+    #m = toBinary(words)
+    #print("WORDS TO BINARY: ", m)    
+    #decword = toString(m)
+    #print("DECWORD  DECODE: ",decword)
+    #print("WORDS TO BINARY: ",m)
+    print("ORIGINAL BEFR: ", m)
+    #m = int(input("enter an int: "))
 
-	decode = pow(c,d,n)
-	print("ORIGINAL TEXT IN: ", decode)
-	return decode
+    decode = pow(c,d,n)
+    print("ORIGINAL TEXT IN: ", decode)
+    return decode
 
 
 if __name__ == "__main__":
